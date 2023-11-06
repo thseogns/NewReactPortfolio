ecd6ff702fef2504bc3e46c275fb6e9680b439ae
thseogns <thseo1758@naver.com>
2023-11-07 06:44:46 +0900

    첫 업로드

--- src/interface/window/myProject/MyProject.tsx
+++ src/interface/window/myProject/MyProject.tsx
@@ -1,9 +1,11 @@
 import React from "react";
 import ProjectItems from "./ProjectItems";
 import styles from "./MyProject.module.css";
+import HoverPage from "./HoverPage";
 const MyProject = () => {
   return (
     <div className={styles.cover}>
+      <HoverPage />
       <ProjectItems />
     </div>
   );
