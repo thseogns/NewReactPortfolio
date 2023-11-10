fd58c05b795405ef4ebbf64e655ba594b59ba4b3
thseogns <thseo1758@naver.com>
2023-11-10 10:53:01 +0900

    Change of address due to moving

--- src/interface/window/information/myHomeMap/MyHomeMap.tsx
+++ src/interface/window/information/myHomeMap/MyHomeMap.tsx
@@ -13,12 +13,12 @@ const MyHomeMap = () => {
     const container = document.getElementById("map"); //지도를 담을 영역의 DOM 레퍼런스
     const options = {
       //지도를 생성할 때 필요한 기본 옵션
-      center: new kakao.maps.LatLng(37.465896, 126.9316562), //지도의 중심좌표.
+      center: new kakao.maps.LatLng(36.9842, 126.9206), //지도의 중심좌표.
       level: 3, //지도의 레벨(확대, 축소 정도)
     };
 
     const map = new kakao.maps.Map(container, options);
-    const markerPosition = new kakao.maps.LatLng(37.465896, 126.9316562);
+    const markerPosition = new kakao.maps.LatLng(36.9842, 126.9206);
     const marker = new kakao.maps.Marker({
       position: markerPosition,
     });
